1eead0a1903087e2884bb469db74a4513eefb025
Jordan Dillard <jordandillard2023@u.northwestern.edu>
2021-11-23 18:22:56 -0600

    (1) Move delete task cypress test to new file

--- cypress/integration/DeleteTask.spec.js
+++ cypress/integration/DeleteTask.spec.js
@@ -1,4 +1,7 @@
-describe('Test App', () => {
+describe('Test task delete button', () => {
+
+    const testTask = "test task";
+    const testPokemon = "Charmander";
 
     it('launches', () => {
         cy.visit('/');
@@ -21,16 +24,27 @@ describe('Test App', () => {
         cy.visit('/');
         cy.get('[data-cy=sign-in]').click();
         cy.get('[data-cy=add-task]').click();
-
-        const testTask = "test task";
-        const testPokemon = "Charmander";
-
         cy.get('input[data-cy="task-input"]')
             .type(testTask);
         cy.get(`[data-cy=${testPokemon}-input]`).click();
         cy.get('[data-cy=submit-btn]').click();
         cy.get('[data-cy=task-name]').should('contain', testTask);
         cy.get('[data-cy=pokemon-name]').should('contain', testPokemon);
+    });
+
+    it('deletes a task', () => {
+        cy.login();
+        cy.visit('/');
+        cy.get('[data-cy=sign-in]').click();
+
+        cy.get('[data-cy=task-name]').should('contain', testTask);
+        cy.get('[data-cy=pokemon-name]').should('contain', testPokemon);
+
+        const delButtonCyName = testTask.replace(' ', '-');
+
+        cy.get(`[data-cy=${testPokemon}-${delButtonCyName}-del-button]`).click();
+        cy.get('[data-cy=task-name]').should('not.exist', testTask);
+        cy.get('[data-cy=pokemon-name]').should('not.exist', testPokemon);
     });
 
     afterEach(() => {
